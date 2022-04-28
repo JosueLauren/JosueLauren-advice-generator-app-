6023e9c837ec3b41662d631ebfae4b4b5f50db05
JosueLauren <josuelauren_@hotmail.com>
2022-04-28 18:40:48 -0300

    refatorando o código para inicio de projeto

---
+++ src/App.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+// import logo from './logo.svg';
+import './App.css';
+
+import { Title, Wrapper } from './components/Teste'
+
+function App() {
+  return (
+    <div className="App">
+      <Wrapper>
+        <Title>
+          Hello World!
+        </Title>
+      </Wrapper>
+    </div>
+  );
+}
+
+export default App;
