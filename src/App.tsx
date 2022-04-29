7ea492cd20ff454341065c7468180b6c4a65d06d
JosueLauren <josuelauren_@hotmail.com>
2022-04-28 21:42:27 -0300

    parte da busca da api finalizada

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,36 @@
+import React from 'react'
 import './App.css';
 
-import {Button, Container, Advice, Paragraph } from './components/Teste'
+import {Button, Container, Advice, Paragraph} from './components/Teste'
+interface ISlip {
+  id: number | null
+  advice: string
+}
+
 
 function App() {
+  const [slip, setSlip] = React.useState<ISlip>({
+    id: null,
+    advice: ''
+  });
+
+  function fetchSlip():void{
+    fetch('https://api.adviceslip.com/advice')
+      .then(result => result.json())
+      .then(data => setSlip(data.slip))
+  }
+
+    React.useEffect(() => {
+      fetchSlip()
+    }, [])
+
   return (
     <div className="App">
       <Container>
-        <Advice>Advice #</Advice>
-        <Paragraph>skksooksoksosoksoksoskoskoskoskoskskksooksoksosoksoksoskoskoskoskoskskksooksoksosoksoksoskoskoskoskosk</Paragraph>
+        <Advice>Advice #{slip.id}</Advice>
+        <Paragraph>{slip.advice}</Paragraph>
         <img src="./imgs/pattern-divider-desktop.svg" width='80%'/>
-        <Button></Button>
+        <Button onClick={() => fetchSlip()} ></Button>
       </Container>
     </div>
   );
